06ef160c057a5eddbd9bc05a6a5eb7bf94410f97
JohanArmando <johan.maxcorp@gmail.com>
2016-09-07 12:29:20 -0500

    Superlog creado como comando de git

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    MELI
                 </div>
 
                 <div class="links">
