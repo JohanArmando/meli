4bb922f5fceb4eef75813135740ccd4265cf20ec
JohanArmando <johan.maxcorp@gmail.com>
2016-09-07 16:14:00 -0500

    MELI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    MELI 
+                    MELI REBASE
                 </div>
 
                 <div class="links">
